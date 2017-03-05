974db0680bf710595a3c6b6b39155be583d92e0d
Thaega <thaega@users.noreply.github.com>
2017-03-05 15:52:17 +0100

    Bah t'en poussin

--- gen.py
+++ gen.py
@@ -131,7 +131,8 @@ debut = ["Qu'est-ce qui est vert et qui tourne en rigolant ?",
          "Pourquoi Perrache ne tient pas debout ?",
          "Pourquoi le clown a-t-il des bretelles bleu-blanc-rouge ?",
          "Qu'est-ce qu'un match de foot à deux ?",
-         "Comment appelle-t-on un lapin qui fait du stand-up ?"]
+         "Comment appelle-t-on un lapin qui fait du stand-up ?",
+         "Il y a 5 poussins sur une table (sur un sol capitonné). Tu veux n'en avoir plus que 4, comment fais-tu ?"]
 
 
 fin = ["Un chou marreur !",
@@ -266,7 +267,8 @@ fin = ["Un chou marreur !",
        "Parce que Vaulx-en-Velin La Soie !",
        "Pour que son pantalon ne tombe pas !",
        "La guerre des goals !",
-       "Un blagomorphe !"]
+       "Un blagomorphe !",
+       "Bah t'en pousses un !"]
 
 def blague(k):
     res = choice(debut)+" "+choice(fin)
