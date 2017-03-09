4248233993ba8e449ffcf9fe0b19f9d0b1a8ab08
Thaega <thaega@users.noreply.github.com>
2017-03-09 14:41:13 +0100

    Blagues

--- gen.py
+++ gen.py
@@ -126,7 +126,11 @@ debut = ["Qu'est-ce qui est vert et qui tourne en rigolant ?",
          "Comment sert-on le thé à un martien ?",
          "Quel est le pluriel de « une bière » ?",
          "Pourquoi la roche volcanique est-elle très propre ?",
+<<<<<<< HEAD
+         "C'est deux chèvres, Babi et Baba, qui sont dans un bateau. Elles tombent à l'eau, que ce passe-t-il ?",
+=======
          "C'est deux chèvres, Babi et Baba, qui sont dans un bateau. Elles tombent à l'eau, que se passe-t-il ?",
+>>>>>>> 913dcb0f34c3af7ab06852e0cefd3173a58ca997
          "Comment appelle-t-on une pyramide avec un masque ?",
          "Pourquoi Perrache ne tient pas debout ?",
          "Pourquoi le clown a-t-il des bretelles bleu-blanc-rouge ?",
@@ -135,10 +139,14 @@ debut = ["Qu'est-ce qui est vert et qui tourne en rigolant ?",
          "Il y a 5 poussins sur une table (sur un sol capitonné). Tu veux n'en avoir plus que 4, comment fais-tu ?",
          "Que font les sardines le samedi soir ?",
          "Quelles sont les deux sortes d'OVNI ?",
+<<<<<<< HEAD
+         "Que dit une vache qui prend ses copines en photo ?"]
+=======
          "Que dit une vache qui prend ses copines en photo ?",
          "Qu'est-ce qu'un steak derrière un arbre ?",
          "J'ai trois têtes, dix bras, quatre jambes et trois pieds. Qui suis-je?",
          "Quelle est la différence entre une autruche et une orange ?"]
+>>>>>>> 913dcb0f34c3af7ab06852e0cefd3173a58ca997
 
 
 fin = ["Un chou marreur !",
@@ -277,10 +285,14 @@ fin = ["Un chou marreur !",
        "Bah t'en pousses un !",
        "Elles vont en boîte !",
        "L'OVNI tender et l'OVNI true.",
+<<<<<<< HEAD
+       "Attention, ne bousez plus !"]
+=======
        "Attention, ne bousez plus !",
        "Un steak caché !",
        "Un menteur.",
        "L'une dort debout tandis que l'autre est un agrume."]
+>>>>>>> 913dcb0f34c3af7ab06852e0cefd3173a58ca997
 
 def blague(k):
     res = choice(debut)+" "+choice(fin)
