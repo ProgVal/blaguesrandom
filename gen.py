23879f806b26a28e154e646e9ed88481d99e39be
Thaega <thaega@users.noreply.github.com>
2017-06-07 14:33:00 +0200

    L'iphone marche (par @krapomusic)

--- gen.py
+++ gen.py
@@ -160,7 +160,8 @@ debut = ["Qu'est-ce qui est vert et qui tourne en rigolant ?",
          "Monsieur et Madame Talu ont 4 fils, comment s'appellent-ils ?",
          "Que dit-on quant Karl Zéro devient Noir ?",
          "Comment appelle-t-on des algébristes qui sont des personnes peu fréquentables ?",
-         "Qu'est-ce que ça fait quand on regarde un angle droit dans un miroir ?"]
+         "Qu'est-ce que ça fait quand on regarde un angle droit dans un miroir ?",
+         "Que s'exclame David Bowie quand son téléphone est enfin rechargé ?"]
 
 fin = ["Un chou marreur !",
        "Parce que Mario brosse !",
@@ -323,7 +324,8 @@ fin = ["Un chou marreur !",
        "Jean, car 4 Jean Talu c'est trop la classe !",
        "On dit que Karl a bruni !",
        "L'algèbre de Lie de l'humanité !",
-       "Un angle gauche !"]
+       "Un angle gauche !",
+       "L'iPhone marche !"]
 
 
 def blague(k):
